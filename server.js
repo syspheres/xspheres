ff564507b166d2fa5f93dbb277d4324eba282f84
SySpheres <syspheres@gmail.com>
2021-05-09 21:35:11 +0200

    create standard folder xspheres main

---
+++ server.js
@@ -0,0 +1,8 @@
+// /server.js
+// Exemple code 
+console.log("server.js :"+" Begin");
+
+// Declare global variable.
+global.HomeDir = __dirname; // home folder of xspheres
+
+console.log("HomeDir "+global.HomeDir);
